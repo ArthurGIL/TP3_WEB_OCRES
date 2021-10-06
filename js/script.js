3600247b823a13b0db02761b248fa23fce224ddc
ArthurGIL <49671280+ArthurGIL@users.noreply.github.com>
2021-10-06 08:59:50 +0200

    Ajout d'une boucle for pour les 3jours

--- js/script.js
+++ js/script.js
@@ -27,7 +27,6 @@ function start() {
       document.getElementById('today-forecast-more-info').innerHTML = description;
       document.getElementById('icon-weather-container').innerHTML = icon;
       document.getElementById('today-forecast-temp').innerHTML = `${temp}°C`;
-      
     })
     .catch(function(error) {
       // Affiche une erreur
@@ -49,41 +48,18 @@ function getThreeDayForecast(){
       const data = response.data;
       console.log(data);
 
-      // On récupère l'information pour le j+1
-      const main1 = data.list[0].weather[0].main;
-      const description1 = data.list[0].weather[0].description;
-      const temp1 = data.list[0].temp.day;
-      const icon1 = apiWeather3Day.getHTMLElementFromIcon(data.list[0].weather[0].icon);
-      // Modifier le DOM
-      document.getElementById('1d-forecast-main').innerHTML = main1;
-      document.getElementById('1d-forecast-more-info').innerHTML = description1;
-      document.getElementById('1d-icon-weather-container').innerHTML = icon1;
-      document.getElementById('1d-forecast-temp').innerHTML = `${temp1}°C`;
+      for(var i=0; i<3; i++) {
+        const main = data.list[i].weather[0].main;
+        const description = data.list[i].weather[0].description;
+        const temp = data.list[i].temp.day;
+        const icon = apiWeather3Day.getHTMLElementFromIcon(data.list[i].weather[0].icon);
 
-
-      // On récupère l'information pour le j+2
-      const main2 = data.list[1].weather[0].main;
-      const description2 = data.list[1].weather[0].description;
-      const temp2 = data.list[1].temp.day;
-      const icon2 = apiWeather3Day.getHTMLElementFromIcon(data.list[1].weather[0].icon);
-      // Modifier le DOM
-      document.getElementById('2d-forecast-main').innerHTML = main2;
-      document.getElementById('2d-forecast-more-info').innerHTML = description2;
-      document.getElementById('2d-icon-weather-container').innerHTML = icon2;
-      document.getElementById('2d-forecast-temp').innerHTML = `${temp2}°C`;
-
-
-      // On récupère l'information pour le j+3
-      const main3 = data.list[2].weather[0].main;
-      const description3 = data.list[2].weather[0].description;
-      const temp3 = data.list[2].temp.day;
-      const icon3 = apiWeather3Day.getHTMLElementFromIcon(data.list[2].weather[0].icon);
-      // Modifier le DOM
-      document.getElementById('3d-forecast-main').innerHTML = main3;
-      document.getElementById('3d-forecast-more-info').innerHTML = description3;
-      document.getElementById('3d-icon-weather-container').innerHTML = icon3;
-      document.getElementById('3d-forecast-temp').innerHTML = `${temp3}°C`;
-      
+        // Modifier le DOM
+        document.getElementById(i + 'd-forecast-main').innerHTML = main;
+        document.getElementById(i + 'd-forecast-more-info').innerHTML = description;
+        document.getElementById(i + 'd-icon-weather-container').innerHTML = icon;
+        document.getElementById(i + 'd-forecast-temp').innerHTML = `${temp}°C`;
+      }
     })
     .catch(function(error) {
       // Affiche une erreur
